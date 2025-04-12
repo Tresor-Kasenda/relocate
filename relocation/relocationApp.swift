8a72c81e7403645212c34e9629e25d400d0bbe04
Tresor kasenda <tresorkasendat@gmail.com>
2025-04-12 12:13:19 +0200

    Initial Commit

---
+++ relocation/relocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  relocationApp.swift
+//  relocation
+//
+//  Created by Tresor KASENDA on 12/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct relocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
